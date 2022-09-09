e0be1ee8ddf21608beea543261ff970f9b5465d6
wahyuricky737 <wahyuricky737@gmail.com>
2022-09-09 17:49:52 +0700

    kris hosting

---
+++ plugins/sewabot.js
@@ -0,0 +1,18 @@
+const sewabot = () => { 
+	return `
+*OPEN JASA SEWA BOT*
+
+Sewa Permanen : 5k
+
+*Payment :*
+_Pulsa Dana Dan Gopay _
+
+Minat?PC
+Wa.me/62882007324217
+`
+}
+handler.help = ['sewa']
+handler.tags = ['info']
+handler.command = /^(sewa)$/i
+
+module.exports = handler
